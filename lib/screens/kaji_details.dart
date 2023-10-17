17d08820bedfc61fe430fb093bfa1fc128cdbadb
Juan Carlos Aguilar Lopez <juandspcf@gmail.com>
2023-10-17 22:52:43 +0900

    mode details added in details screen

--- lib/screens/kaji_details.dart
+++ lib/screens/kaji_details.dart
@@ -14,7 +14,7 @@ class KanjiDetails extends StatefulWidget {
 }
 
 class KanjiDetailsState extends State<KanjiDetails> {
-  late VideoPlayerController _controller;
+  late VideoPlayerController _videoController;
   final assetsAudioPlayer = AssetsAudioPlayer();
 
   String capitalizeString(String text) {
@@ -25,31 +25,38 @@ class KanjiDetailsState extends State<KanjiDetails> {
 
   List<Widget> getListStrokes(List<String> images) {
     List<Widget> strokes = [];
-    for (int i = 0; i < images.length; i++) {
+    for (int index = 0; index < images.length; index++) {
       Widget stroke = Row(
         children: [
           const SizedBox(
             width: 20,
           ),
           Container(
-            color: Colors.white70,
             height: 80,
             width: 80,
-            child: SvgPicture.network(
-              images[i],
-              height: 80,
-              width: 80,
-              semanticsLabel: widget.kanjiFromApi.kanjiCharacter,
-              placeholderBuilder: (BuildContext context) => Container(
-                  color: Colors.transparent,
-                  height: 40,
-                  width: 40,
-                  child: const CircularProgressIndicator(
-                    backgroundColor: Color.fromARGB(179, 5, 16, 51),
-                  )),
+            decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.onPrimaryContainer,
+                borderRadius: const BorderRadius.all(Radius.circular(10))),
+            child: GestureDetector(
+              onTap: () {
+                _scaleDialog(images[index], index);
+              },
+              child: SvgPicture.network(
+                images[index],
+                height: 80,
+                width: 80,
+                semanticsLabel: widget.kanjiFromApi.kanjiCharacter,
+                placeholderBuilder: (BuildContext context) => Container(
+                    color: Colors.transparent,
+                    height: 40,
+                    width: 40,
+                    child: const CircularProgressIndicator(
+                      backgroundColor: Color.fromARGB(179, 5, 16, 51),
+                    )),
+              ),
             ),
           ),
-          i != images.length - 1
+          index != images.length - 1
               ? const SizedBox(
                   width: 10,
                   child: Icon(Icons.arrow_circle_right_outlined),
@@ -64,16 +71,118 @@ class KanjiDetailsState extends State<KanjiDetails> {
     return strokes;
   }
 
+  List<Widget> getIndexedExamples(
+      List<Examples> examples, Function() stopAnimation) {
+    List<Widget> indexedExamples = [];
+    for (int index = 0; index < examples.length; index++) {
+      final tileWidget = ListTile(
+        leading: Text(
+          '${index + 1}._',
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        title: Column(
+          children: [
+            Text(examples[index].japanese),
+            const SizedBox(
+              height: 7,
+            ),
+            Text(examples[index].meaning.english),
+            const SizedBox(
+              height: 7,
+            ),
+          ],
+        ),
+        trailing: Container(
+          decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+              borderRadius: BorderRadius.circular(30)),
+          child: IconButton(
+            color: Theme.of(context).colorScheme.onPrimary,
+            onPressed: () async {
+              stopAnimation();
+
+              final assetsAudioPlayer = AssetsAudioPlayer();
+
+              try {
+                await assetsAudioPlayer.open(
+                  Audio.network(examples[index].audio.mp3),
+                );
+              } catch (t) {
+                //mp3 unreachable
+              }
+            },
+            icon: const Icon(Icons.play_arrow_rounded),
+          ),
+        ),
+      );
+
+      indexedExamples.add(tileWidget);
+    }
+
+    return indexedExamples;
+  }
+
+  void stopAnimation() {
+    setState(() {
+      if (_videoController.value.isPlaying) _videoController.pause();
+    });
+  }
+
+  Widget _dialog(BuildContext context, String image, int index) {
+    return AlertDialog(
+      title: Text("Stroke number ${index + 1}"),
+      content: SvgPicture.network(
+        image,
+        height: 120,
+        width: 120,
+        colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+        //color: Colors.white,
+        semanticsLabel: widget.kanjiFromApi.kanjiCharacter,
+        placeholderBuilder: (BuildContext context) => Container(
+            color: Colors.transparent,
+            height: 80,
+            width: 80,
+            child: const CircularProgressIndicator(
+              backgroundColor: Color.fromARGB(179, 5, 16, 51),
+            )),
+      ),
+      actions: <Widget>[
+        TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text("Okay"))
+      ],
+    );
+  }
+
+  void _scaleDialog(String image, int index) {
+    showGeneralDialog(
+      context: context,
+      pageBuilder: (ctx, a1, a2) {
+        return Container();
+      },
+      transitionBuilder: (ctx, a1, a2, child) {
+        var curve = Curves.easeInOut.transform(a1.value);
+        return Transform.scale(
+          scale: curve,
+          child: _dialog(ctx, image, index),
+        );
+      },
+      transitionDuration: const Duration(milliseconds: 300),
+    );
+  }
+
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.networkUrl(
+    _videoController = VideoPlayerController.networkUrl(
         Uri.parse(widget.kanjiFromApi.videoLink))
       ..initialize().then((_) {
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {
-          _controller.setLooping(true);
-          _controller.play();
+          _videoController.setLooping(true);
+          _videoController.play();
         });
       });
   }
@@ -81,8 +190,8 @@ class KanjiDetailsState extends State<KanjiDetails> {
   @override
   void dispose() {
     super.dispose();
-    _controller.pause();
-    _controller.dispose();
+    _videoController.pause();
+    _videoController.dispose();
   }
 
   @override
@@ -96,17 +205,43 @@ class KanjiDetailsState extends State<KanjiDetails> {
           const SizedBox(
             height: 20,
           ),
-          _controller.value.isInitialized
+          _videoController.value.isInitialized
               ? Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
                     SizedBox(
-                      height: 128 * _controller.value.aspectRatio,
-                      width: 128,
+                      height: 100 * _videoController.value.aspectRatio,
+                      width: 100,
                       child: AspectRatio(
-                        aspectRatio: _controller.value.aspectRatio,
-                        child: VideoPlayer(_controller),
+                        aspectRatio: _videoController.value.aspectRatio,
+                        child: VideoPlayer(_videoController),
                       ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.primary,
+                          borderRadius: BorderRadius.circular(30)),
+                      child: IconButton(
+                        color: Theme.of(context).colorScheme.onPrimary,
+                        onPressed: () {
+                          setState(() {
+                            _videoController.value.isPlaying
+                                ? _videoController.pause()
+                                : _videoController.play();
+                          });
+                        },
+                        icon: Icon(
+                          _videoController.value.isPlaying
+                              ? Icons.pause
+                              : Icons.play_arrow,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
                     ),
                   ],
                 )
@@ -114,7 +249,7 @@ class KanjiDetailsState extends State<KanjiDetails> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     SizedBox(
-                        height: 128 * _controller.value.aspectRatio,
+                        height: 128 * _videoController.value.aspectRatio,
                         width: 128,
                         child: const Padding(
                           padding: EdgeInsets.all(40.0),
@@ -123,28 +258,69 @@ class KanjiDetailsState extends State<KanjiDetails> {
                   ],
                 ),
           const SizedBox(
-            height: 20,
+            height: 10,
+          ),
+          const Divider(
+            height: 4,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Strokes",
+            style: Theme.of(context)
+                .textTheme
+                .titleLarge!
+                .copyWith(fontWeight: FontWeight.bold),
           ),
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [...getListStrokes(widget.kanjiFromApi.strokes.images)],
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ...getListStrokes(widget.kanjiFromApi.strokes.images)
+                  ],
+                ),
+              ),
             ),
           ),
           const SizedBox(
-            height: 20,
+            height: 10,
+          ),
+          const Divider(
+            height: 4,
+          ),
+          const SizedBox(
+            height: 10,
           ),
           Text(
-            capitalizeString(widget.kanjiFromApi.englishMeaning),
+            "Meaning and definition",
             style: Theme.of(context)
                 .textTheme
                 .titleLarge!
                 .copyWith(fontWeight: FontWeight.bold),
           ),
           const SizedBox(
-            height: 20,
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(
+                width: 20,
+              ),
+              Text(
+                capitalizeString(
+                    'Meaning: ${widget.kanjiFromApi.englishMeaning}'),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.start,
@@ -168,34 +344,47 @@ class KanjiDetailsState extends State<KanjiDetails> {
             ],
           ),
           const SizedBox(
-            height: 20,
+            height: 10,
           ),
-          ExamplesAndInfo(widget: widget),
+          const Divider(
+            height: 4,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Examples",
+            style: Theme.of(context)
+                .textTheme
+                .titleLarge!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  ...getIndexedExamples(
+                      widget.kanjiFromApi.example, stopAnimation)
+                ],
+              ),
+            ),
+          )
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _controller.value.isPlaying
-                ? _controller.pause()
-                : _controller.play();
-          });
-        },
-        child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-        ),
       ),
     );
   }
 }
 
 class ExamplesAndInfo extends StatelessWidget {
-  const ExamplesAndInfo({
-    super.key,
-    required this.widget,
-  });
+  const ExamplesAndInfo(
+      {super.key, required this.widget, required this.stopAnimation});
 
   final KanjiDetails widget;
+  final void Function() stopAnimation;
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -222,6 +411,8 @@ class ExamplesAndInfo extends StatelessWidget {
                 child: IconButton(
                   color: Theme.of(context).colorScheme.onPrimary,
                   onPressed: () async {
+                    stopAnimation();
+
                     final assetsAudioPlayer = AssetsAudioPlayer();
 
                     try {
