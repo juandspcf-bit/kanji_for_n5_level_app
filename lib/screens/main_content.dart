5c96ed35d4f36c22f029bcd9d153759ef3f56f96
Juan Carlos Aguilar Lopez <juandspcf@gmail.com>
2023-10-18 19:13:31 +0900

    added bottom bar navigation

--- lib/screens/main_content.dart
+++ lib/screens/main_content.dart
@@ -2,8 +2,21 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:kanji_for_n5_level_app/screens/sections_screen.dart';
 
-class MainContent extends StatelessWidget {
+class MainContent extends StatefulWidget {
   const MainContent({super.key});
+
+  @override
+  State<MainContent> createState() => _MainContentState();
+}
+
+class _MainContentState extends State<MainContent> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +42,24 @@ class MainContent extends StatelessWidget {
         children: [
           Expanded(
             child: Sections(),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+        selectedItemColor: Theme.of(context).colorScheme.onSecondaryContainer,
+        unselectedItemColor:
+            Theme.of(context).colorScheme.onSecondaryContainer.withAlpha(100),
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: "Categories",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: "Favorites",
           ),
         ],
       ),
