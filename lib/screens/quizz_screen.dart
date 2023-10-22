096a1ff1a4b075a408bbda8be282d0e2244118fa
Juan Carlos Aguilar Lopez <juandspcf@gmail.com>
2023-10-22 21:35:56 +0900

    second attemp to make a draggable quiz

--- lib/screens/quizz_screen.dart
+++ lib/screens/quizz_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:kanji_for_n5_level_app/models/kanji_from_api.dart';
 
 class QuizScreen extends StatefulWidget {
@@ -14,7 +15,8 @@ class QuizScreen extends StatefulWidget {
 }
 
 class _QuizScreenState extends State<QuizScreen> {
-  String kanjiDragged = '';
+  String kanjiImageLinkDragged = '';
+  double opacityDragged = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,21 +27,45 @@ class _QuizScreenState extends State<QuizScreen> {
             children: [
               Padding(
                 padding:
-                    const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+                    const EdgeInsets.symmetric(vertical: 50, horizontal: 30),
                 child: Draggable<KanjiFromApi>(
                   data: widget.kanjisModel[0],
                   feedback: Text(widget.kanjisModel[0].kanjiCharacter),
                   childWhenDragging: Container(
                     width: 70,
                     height: 70,
-                    color: const Color.fromARGB(97, 255, 193, 7),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context)
+                          .colorScheme
+                          .onPrimaryContainer
+                          .withOpacity(0.3),
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
                   ),
                   child: Container(
                     width: 70,
                     height: 70,
-                    color: Colors.amber,
-                    child: Center(
-                        child: Text(widget.kanjisModel[0].kanjiCharacter)),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.onPrimaryContainer,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    child: SvgPicture.network(
+                      widget.kanjisModel[0].kanjiImageLink,
+                      height: 70,
+                      width: 70,
+                      semanticsLabel: widget.kanjisModel[0].kanjiCharacter,
+                      placeholderBuilder: (BuildContext context) => Container(
+                          color: Colors.transparent,
+                          height: 40,
+                          width: 40,
+                          child: const CircularProgressIndicator(
+                            backgroundColor: Color.fromARGB(179, 5, 16, 51),
+                          )),
+                    ),
                   ),
                 ),
               ),
@@ -50,17 +76,57 @@ class _QuizScreenState extends State<QuizScreen> {
             children: [
               Padding(
                 padding:
-                    const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+                    const EdgeInsets.symmetric(vertical: 50, horizontal: 30),
                 child: DragTarget<KanjiFromApi>(onAccept: (data) {
                   setState(() {
-                    kanjiDragged = data.kanjiCharacter;
+                    kanjiImageLinkDragged = data.kanjiImageLink;
+                    opacityDragged = 1.0;
                   });
                 }, builder: (ctx, _, __) {
-                  return Container(
-                    width: 70,
-                    height: 70,
-                    color: Colors.amber,
-                    child: Center(child: Text(kanjiDragged)),
+                  return Column(
+                    children: [
+                      Container(
+                        width: 70,
+                        height: 70,
+                        decoration: BoxDecoration(
+                          color: Theme.of(context)
+                              .colorScheme
+                              .onPrimaryContainer
+                              .withOpacity(opacityDragged),
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                        child: SvgPicture.network(
+                          kanjiImageLinkDragged,
+                          height: 70,
+                          width: 70,
+                          semanticsLabel: widget.kanjisModel[0].kanjiCharacter,
+                          placeholderBuilder: (BuildContext context) =>
+                              Container(
+                            color: Colors.transparent,
+                            height: 40,
+                            width: 40,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      const Divider(
+                        thickness: 10.5,
+                        height: 5,
+                        color: Colors.white,
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(widget.kanjisModel[0].englishMeaning),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(widget.kanjisModel[0].hiraganaMeaning)
+                    ],
                   );
                 }),
               ),
