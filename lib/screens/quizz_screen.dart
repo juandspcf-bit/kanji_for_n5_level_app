7f9b0232f53559a74a79880c4377ec35f1d53114
juandspcf-bit <juandspcf@gmail.com>
2023-10-23 14:50:03 +0900

    added styling to the dragged box

--- lib/screens/quizz_screen.dart
+++ lib/screens/quizz_screen.dart
@@ -17,26 +17,35 @@ class QuizScreen extends StatefulWidget {
 class _QuizScreenState extends State<QuizScreen> {
   late List<String> kanjiImageLinksDrag;
   late List<double> initialOpacities;
-  late (List<KanjiFromApi>, List<KanjiFromApi>) randomData;
+  late List<bool> isDraggeInitialList;
+  late List<KanjiFromApi> randomData;
+  late List<KanjiFromApi> randomSolutions;
+  late int index;
 
-  (List<String>, List<double>) initLinks(int lenght) {
+  (List<String>, List<double>, List<bool>) initLinks(int lenght) {
     final initialLinks = <String>[];
     final initialOpacities = <double>[];
+    final isDraggedList = <bool>[];
     for (int i = 0; i < lenght; i++) {
       initialLinks.add("");
       initialOpacities.add(0.0);
+      isDraggedList.add(false);
     }
-    return (initialLinks, initialOpacities);
+    return (initialLinks, initialOpacities, isDraggedList);
   }
 
-  (List<KanjiFromApi>, List<KanjiFromApi>) suffleData() {
+  List<KanjiFromApi> suffleData() {
     final copy1 = [...widget.kanjisModel];
-    final copy2 = [...widget.kanjisModel];
-
     copy1.shuffle();
-    copy2.shuffle();
+    return copy1;
+  }
 
-    return (copy1, copy2);
+  List<KanjiFromApi> getPosibleSolutions(KanjiFromApi kanjiToRemove) {
+    final copy1 = [...randomData];
+    copy1.remove(kanjiToRemove);
+    final copy2 = [kanjiToRemove, ...copy1.sublist(0, 2)];
+    copy2.shuffle();
+    return copy2;
   }
 
   @override
@@ -45,83 +54,110 @@ class _QuizScreenState extends State<QuizScreen> {
     final initValues = initLinks(widget.kanjisModel.length);
     kanjiImageLinksDrag = initValues.$1;
     initialOpacities = initValues.$2;
+    isDraggeInitialList = initValues.$3;
     randomData = suffleData();
+    index = 0;
+    randomSolutions = getPosibleSolutions(randomData[0]);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Test your knowledge")),
-      body: SingleChildScrollView(
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 0,
+          bottom: 0,
+          right: 30,
+          left: 30,
+        ),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            for (int i = 0; i < randomData.$1.length; i++)
-              Padding(
-                padding: EdgeInsets.only(
-                  top: i == 0 ? 50 : 20,
-                  bottom: 0,
-                  right: 30,
-                  left: 30,
-                ),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Draggable<KanjiFromApi>(
-                      data: randomData.$1[i],
-                      feedback: Text(randomData.$1[i].kanjiCharacter),
-                      childWhenDragging: Container(
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                isDraggeInitialList[index] == true
+                    ? Container(
                         width: 70,
                         height: 70,
                         decoration: BoxDecoration(
                           color: Theme.of(context)
                               .colorScheme
                               .onPrimaryContainer
-                              .withOpacity(0.3),
+                              .withOpacity(0),
                           borderRadius: const BorderRadius.all(
                             Radius.circular(10),
                           ),
+                          border: Border.all(color: Colors.white),
                         ),
-                      ),
-                      child: Container(
-                        width: 70,
-                        height: 70,
-                        decoration: BoxDecoration(
-                          color:
-                              Theme.of(context).colorScheme.onPrimaryContainer,
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(10),
-                          ),
-                        ),
-                        child: SvgPicture.network(
-                          randomData.$1[i].kanjiImageLink,
-                          height: 70,
+                      )
+                    : Draggable<KanjiFromApi>(
+                        data: randomData[index],
+                        feedback: Text(randomData[index].kanjiCharacter),
+                        childWhenDragging: Container(
                           width: 70,
-                          semanticsLabel: randomData.$1[i].kanjiCharacter,
-                          placeholderBuilder: (BuildContext context) =>
-                              Container(
-                                  color: Colors.transparent,
-                                  height: 40,
-                                  width: 40,
-                                  child: const CircularProgressIndicator(
-                                    backgroundColor:
-                                        Color.fromARGB(179, 5, 16, 51),
-                                  )),
+                          height: 70,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onPrimaryContainer
+                                .withOpacity(0),
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                            border: Border.all(color: Colors.white),
+                          ),
+                        ),
+                        child: Container(
+                          width: 70,
+                          height: 70,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onPrimaryContainer,
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                          child: SvgPicture.network(
+                            randomData[index].kanjiImageLink,
+                            height: 70,
+                            width: 70,
+                            semanticsLabel: randomData[index].kanjiCharacter,
+                            placeholderBuilder: (BuildContext context) =>
+                                Container(
+                                    color: Colors.transparent,
+                                    height: 40,
+                                    width: 40,
+                                    child: const CircularProgressIndicator(
+                                      backgroundColor:
+                                          Color.fromARGB(179, 5, 16, 51),
+                                    )),
+                          ),
                         ),
                       ),
-                    ),
-                    const Spacer(),
-                    DragTarget<KanjiFromApi>(onAccept: (data) {
-                      setState(() {
-                        kanjiImageLinksDrag[i] = data.kanjiImageLink;
-                        initialOpacities[i] = 1.0;
-                      });
-                    }, builder: (ctx, _, __) {
-                      return Column(
-                        children: [
-                          Container(
-                            width: 70,
-                            height: 70,
-                            decoration: BoxDecoration(
+                const Spacer(),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    for (int i = 0; i < randomSolutions.length; i++)
+                      DragTarget<KanjiFromApi>(onAccept: (data) {
+                        setState(() {
+                          kanjiImageLinksDrag[i] = data.kanjiImageLink;
+                          initialOpacities[i] = 1.0;
+                          isDraggeInitialList[index] = true;
+                        });
+                      }, builder: (ctx, _, __) {
+                        return Column(
+                          children: [
+                            Container(
+                              width: 70,
+                              height: 70,
+                              decoration: BoxDecoration(
                                 color: Theme.of(context)
                                     .colorScheme
                                     .onPrimaryContainer
@@ -129,34 +165,56 @@ class _QuizScreenState extends State<QuizScreen> {
                                 borderRadius: const BorderRadius.all(
                                   Radius.circular(10),
                                 ),
-                                border: Border.all(color: Colors.white)),
-                            child: SvgPicture.network(
-                              kanjiImageLinksDrag[i],
-                              height: 70,
-                              width: 70,
-                              semanticsLabel: randomData.$2[i].kanjiCharacter,
-                              placeholderBuilder: (BuildContext context) =>
-                                  Container(
-                                color: Colors.transparent,
-                                height: 40,
-                                width: 40,
+                                border: Border.all(color: Colors.white),
+                              ),
+                              child: SvgPicture.network(
+                                kanjiImageLinksDrag[i],
+                                height: 70,
+                                width: 70,
+                                semanticsLabel:
+                                    randomSolutions[i].kanjiCharacter,
+                                placeholderBuilder: (BuildContext context) =>
+                                    Container(
+                                  color: Colors.transparent,
+                                  height: 40,
+                                  width: 40,
+                                ),
                               ),
                             ),
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Text(randomData.$2[i].englishMeaning),
-                          const SizedBox(
-                            height: 2,
-                          ),
-                          Text(randomData.$2[i].hiraganaMeaning)
-                        ],
-                      );
-                    }),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(randomSolutions[i].englishMeaning),
+                            const SizedBox(
+                              height: 2,
+                            ),
+                            Text(randomSolutions[i].hiraganaMeaning)
+                          ],
+                        );
+                      }),
                   ],
                 ),
-              ),
+              ],
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.arrow_circle_left),
+                  label: const Text('previous'),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.arrow_circle_right),
+                  label: const Text('next'),
+                  //style: ElevatedButtonTheme.of(context).style!.copyWith(minimumSize: 150),
+                )
+              ],
+            )
           ],
         ),
       ),
