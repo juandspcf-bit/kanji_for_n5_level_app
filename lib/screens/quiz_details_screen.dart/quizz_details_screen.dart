8ba3b72b83ea19a89442712cb95ec1f0caddbe74
Juan Carlos Aguilar lopez <juandscf@gmail.com>
2023-12-16 21:48:06 +0900

    correctly handle of offline connection in quiz details screen

--- lib/screens/quiz_details_screen.dart/quizz_details_screen.dart
+++ lib/screens/quiz_details_screen.dart/quizz_details_screen.dart
@@ -1,32 +1,36 @@
-import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kanji_for_n5_level_app/models/kanji_from_api.dart';
 import 'package:kanji_for_n5_level_app/providers/select_quiz_details_screen.dart';
+import 'package:kanji_for_n5_level_app/providers/status_connection_provider.dart';
+import 'package:kanji_for_n5_level_app/providers/status_stored_provider.dart';
+import 'package:kanji_for_n5_level_app/screens/kanji_details/error_connection_tabs.dart';
 import 'package:kanji_for_n5_level_app/screens/quiz_details_screen.dart/flash_card_screen.dart';
 import 'package:kanji_for_n5_level_app/screens/quiz_details_screen.dart/quiz_details_question.dart';
 import 'package:kanji_for_n5_level_app/screens/quiz_details_screen.dart/quiz_details_score.dart';
 import 'package:kanji_for_n5_level_app/screens/quiz_details_screen.dart/welcome_kanji_details_quiz_screen.dart';
 
-class QuizDetailsScreen extends ConsumerStatefulWidget {
+class QuizDetailsScreen extends ConsumerWidget {
   const QuizDetailsScreen({super.key, required this.kanjiFromApi});
 
   final KanjiFromApi kanjiFromApi;
 
-  @override
-  ConsumerState<QuizDetailsScreen> createState() => _QuizDetailsScreenState();
-}
-
-class _QuizDetailsScreenState extends ConsumerState<QuizDetailsScreen> {
-  Widget _selectScreen(ScreensQuizDetail screenNumber) {
-    if (screenNumber == ScreensQuizDetail.welcome) {
+  Widget _selectScreen(
+      ConnectivityResult resultStatus, ScreensQuizDetail screenNumber) {
+    if (resultStatus == ConnectivityResult.none &&
+        kanjiFromApi.statusStorage == StatusStorage.onlyOnline) {
+      return const ErrorConnectionScreen(
+        message: 'The internet connection has gone, restart the quiz later',
+      );
+    } else if (screenNumber == ScreensQuizDetail.welcome) {
       return const WelcomeKanjiDetailsQuizScreen();
     } else if (screenNumber == ScreensQuizDetail.quizSelections) {
-      return QuestionScreen(kanjiFromApi: widget.kanjiFromApi);
+      return QuestionScreen(kanjiFromApi: kanjiFromApi);
     } else if (screenNumber == ScreensQuizDetail.scoreSelections) {
       return const QuizDetailsScore();
     } else if (screenNumber == ScreensQuizDetail.quizFlashCard) {
-      return FlassCardScreen(kanjiFromApi: widget.kanjiFromApi);
+      return FlassCardScreen(kanjiFromApi: kanjiFromApi);
     } else {
       return const Center(
         child: Text('Error'),
@@ -34,9 +38,9 @@ class _QuizDetailsScreenState extends ConsumerState<QuizDetailsScreen> {
     }
   }
 
-  final assetsAudioPlayer = AssetsAudioPlayer();
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final resultStatus = ref.watch(statusConnectionProvider);
     final screenNumber = ref.watch(selectQuizDetailsProvider);
     return Scaffold(
       appBar: AppBar(
@@ -44,7 +48,7 @@ class _QuizDetailsScreenState extends ConsumerState<QuizDetailsScreen> {
           'Test your knowledge',
         ),
       ),
-      body: _selectScreen(screenNumber.screensQuizDetail),
+      body: _selectScreen(resultStatus, screenNumber.screensQuizDetail),
     );
   }
 }
