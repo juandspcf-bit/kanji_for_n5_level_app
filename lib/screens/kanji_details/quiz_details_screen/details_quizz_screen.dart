55795309b6bf06251d0b0133348342065280e711
Juan Carlos Aguilar Lopez <juandspcf@gmail.com>
2024-01-20 18:23:21 +0900

    removed unncesary code for quiz details screen

--- lib/screens/kanji_details/quiz_details_screen/details_quizz_screen.dart
+++ lib/screens/kanji_details/quiz_details_screen/details_quizz_screen.dart
@@ -1,19 +1,8 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kanji_for_n5_level_app/main.dart';
 import 'package:kanji_for_n5_level_app/models/kanji_from_api.dart';
-import 'package:kanji_for_n5_level_app/providers/quiz_details_score_screen.dart';
-import 'package:kanji_for_n5_level_app/providers/select_quiz_details_screen.dart';
-import 'package:kanji_for_n5_level_app/providers/status_connection_provider.dart';
-import 'package:kanji_for_n5_level_app/providers/status_stored_provider.dart';
-import 'package:kanji_for_n5_level_app/screens/common_screens.dart/error_connection_screen.dart';
 import 'package:kanji_for_n5_level_app/screens/kanji_details/quiz_details_screen/flash_card/flash_card_quiz_provider.dart';
-import 'package:kanji_for_n5_level_app/screens/navigation_bar_screens/sections_screen/section_screen_provider.dart';
-import 'package:kanji_for_n5_level_app/screens/kanji_details/quiz_details_screen/flash_card/flash_card_screen.dart';
-import 'package:kanji_for_n5_level_app/screens/kanji_details/quiz_details_screen/last_score_details_provider.dart';
-import 'package:kanji_for_n5_level_app/screens/kanji_details/quiz_details_screen/quiz_details_question.dart';
-import 'package:kanji_for_n5_level_app/screens/kanji_details/quiz_details_screen/quiz_details_score.dart';
 import 'package:kanji_for_n5_level_app/screens/kanji_details/quiz_details_screen/welcome_kanji_details_quiz_screen.dart';
 
 class DetailsQuizScreen extends ConsumerWidget {
@@ -46,9 +35,6 @@ class DetailsQuizScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final resultStatus = ref.watch(statusConnectionProvider);
-    final screenNumber = ref.watch(selectQuizDetailsProvider);
-    final scores = ref.watch(quizDetailsScoreProvider);
     return PopScope(
       onPopInvoked: (didPop) {
         if (didPop) {
@@ -63,44 +49,8 @@ class DetailsQuizScreen extends ConsumerWidget {
           ),
         ),
         //body: _selectScreen(resultStatus, screenNumber.screensQuizDetail),
-        body: Builder(
-          builder: (context) {
-            if (resultStatus == ConnectivityResult.none &&
-                kanjiFromApi.statusStorage == StatusStorage.onlyOnline) {
-              return const ErrorConnectionScreen(
-                message:
-                    'The internet connection has gone, restart the quiz later',
-              );
-            } else if (screenNumber.screensQuizDetail ==
-                ScreensQuizDetail.welcome) {
-              return WelcomeKanjiDetailsQuizScreen(kanjiFromApi: kanjiFromApi);
-            } else if (screenNumber.screensQuizDetail ==
-                ScreensQuizDetail.quizSelections) {
-              return QuestionScreen(kanjiFromApi: kanjiFromApi);
-            } else if (screenNumber.screensQuizDetail ==
-                ScreensQuizDetail.scoreSelections) {
-              ref.read(lastScoreDetailsProvider.notifier).setFinishedQuiz(
-                    section: ref.read(sectionProvider),
-                    uuid: authService.user ?? '',
-                    kanjiCharacter: kanjiFromApi.kanjiCharacter,
-                    countCorrects: scores.correctAnswers.length,
-                    countIncorrects: scores.incorrectAnwers.length,
-                    countOmited: scores.omitted.length,
-                  );
-              return const QuizDetailsScore();
-            } else if (screenNumber.screensQuizDetail ==
-                ScreensQuizDetail.quizFlashCard) {
-              ref.read(flashCardProvider.notifier).answers = List<bool>.filled(
-                  kanjiFromApi.example.length, false,
-                  growable: true);
-              logger.d(ref.read(flashCardProvider.notifier).answers.length);
-              return FlashCardScreen(kanjiFromApi: kanjiFromApi);
-            } else {
-              return const Center(
-                child: Text('Error'),
-              );
-            }
-          },
+        body: WelcomeKanjiDetailsQuizScreen(
+          kanjiFromApi: kanjiFromApi,
         ),
       ),
     );
