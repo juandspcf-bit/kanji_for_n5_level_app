f657e7b87f68ef8b2a51f3e8cf5529516b81d9d6
Juan Carlos Aguilar Lopez <juandspcf@gmail.com>
2024-01-29 19:03:00 +0900

    refactoring of current repos again

--- lib/aplication_layer/repository_contract/api_repo/kanji_kanji_alive_api_impl.dart
+++ lib/aplication_layer/repository_contract/api_repo/kanji_kanji_alive_api_impl.dart
@@ -1,4 +1,4 @@
-import 'package:kanji_for_n5_level_app/aplication_layer/repository_contract/kanji_api_contract.dart';
+import 'package:kanji_for_n5_level_app/aplication_layer/repository_contract/api_repo/kanji_api_contract.dart';
 import 'package:kanji_for_n5_level_app/models/kanji_from_api.dart';
 import 'package:kanji_for_n5_level_app/repositories/apis/kanji_alive/request_kanji_list_api.dart';
 
