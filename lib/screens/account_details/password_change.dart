0bc30d016bf756735be8538e321a15641df65043
Juan Carlos Aguilar Lopez <juandspcf@gmail.com>
2024-02-13 22:28:08 +0900

    added scroll to password widget

--- lib/screens/account_details/password_change.dart
+++ lib/screens/account_details/password_change.dart
@@ -33,110 +33,119 @@ class PassworChange extends ConsumerWidget {
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Column(
-      children: [
-        Icon(
-          Icons.key,
-          color: Theme.of(context).colorScheme.primary,
-          size: 100,
-        ),
-        const SizedBox(
-          height: 40,
-        ),
-        Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  initialValue: initPassword,
-                  obscureText: !isVisiblePassword,
-                  decoration: InputDecoration(
-                    label: const Text('password'),
-                    suffixIcon: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: GestureDetector(
-                        onTap: () {
-                          final currentState = _formKey.currentState;
-                          if (currentState == null) return;
-                          currentState.save();
-                          ref
-                              .read(passwordChangeFlowProvider.notifier)
-                              .toggleVisibilityPassword();
-                        },
-                        child: isVisiblePassword
-                            ? const Icon(Icons.visibility_off)
-                            : const Icon(Icons.visibility),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Icon(
+            Icons.key,
+            color: Theme.of(context).colorScheme.primary,
+            size: 100,
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    initialValue: initPassword,
+                    obscureText: !isVisiblePassword,
+                    decoration: InputDecoration(
+                      label: const Text('password'),
+                      suffixIcon: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            final currentState = _formKey.currentState;
+                            if (currentState == null) return;
+                            currentState.save();
+                            ref
+                                .read(passwordChangeFlowProvider.notifier)
+                                .toggleVisibilityPassword();
+                          },
+                          child: isVisiblePassword
+                              ? const Icon(Icons.visibility_off)
+                              : const Icon(Icons.visibility),
+                        ),
                       ),
+                      border: const OutlineInputBorder(),
                     ),
-                    border: const OutlineInputBorder(),
+                    validator: (text) {
+                      if (text != null &&
+                          text.length >= 4 &&
+                          text.length <= 20) {
+                        return null;
+                      } else {
+                        return 'Password should be between 20 and 4 characters';
+                      }
+                    },
+                    onSaved: (text) {
+                      ref
+                          .read(passwordChangeFlowProvider.notifier)
+                          .setPassword(text ?? '');
+                    },
                   ),
-                  validator: (text) {
-                    if (text != null && text.length >= 4 && text.length <= 20) {
-                      return null;
-                    } else {
-                      return 'Password should be between 20 and 4 characters';
-                    }
-                  },
-                  onSaved: (text) {
-                    ref
-                        .read(passwordChangeFlowProvider.notifier)
-                        .setPassword(text ?? '');
-                  },
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  initialValue: initConfirmPassword,
-                  obscureText: !isVisibleConfirmPassword,
-                  decoration: InputDecoration(
-                    label: const Text('confirm your password'),
-                    suffixIcon: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: GestureDetector(
-                        onTap: () {
-                          final currentState = _formKey.currentState;
-                          if (currentState == null) return;
-                          currentState.save();
-                          ref
-                              .read(passwordChangeFlowProvider.notifier)
-                              .toggleConfirmVisibilityPassword();
-                        },
-                        child: isVisibleConfirmPassword
-                            ? const Icon(Icons.visibility_off)
-                            : const Icon(Icons.visibility),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    initialValue: initConfirmPassword,
+                    obscureText: !isVisibleConfirmPassword,
+                    decoration: InputDecoration(
+                      label: const Text('confirm your password'),
+                      suffixIcon: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            final currentState = _formKey.currentState;
+                            if (currentState == null) return;
+                            currentState.save();
+                            ref
+                                .read(passwordChangeFlowProvider.notifier)
+                                .toggleConfirmVisibilityPassword();
+                          },
+                          child: isVisibleConfirmPassword
+                              ? const Icon(Icons.visibility_off)
+                              : const Icon(Icons.visibility),
+                        ),
                       ),
+                      border: const OutlineInputBorder(),
                     ),
-                    border: const OutlineInputBorder(),
+                    validator: (text) {
+                      if (text != null &&
+                          text.length >= 4 &&
+                          text.length <= 20) {
+                        return null;
+                      } else {
+                        return 'Password should be between 20 and 4 characters';
+                      }
+                    },
+                    onSaved: (text) {
+                      ref
+                          .read(passwordChangeFlowProvider.notifier)
+                          .setConfirmPassword(text ?? '');
+                    },
                   ),
-                  validator: (text) {
-                    if (text != null && text.length >= 4 && text.length <= 20) {
-                      return null;
-                    } else {
-                      return 'Password should be between 20 and 4 characters';
-                    }
-                  },
-                  onSaved: (text) {
-                    ref
-                        .read(passwordChangeFlowProvider.notifier)
-                        .setConfirmPassword(text ?? '');
-                  },
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    onValidation(ref);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size.fromHeight(40),
+                  const SizedBox(
+                    height: 20,
                   ),
-                  child: const Text('change your password'),
-                ),
-              ],
-            ))
-      ],
+                  ElevatedButton(
+                    onPressed: () {
+                      onValidation(ref);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(40),
+                    ),
+                    child: const Text('change your password'),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ))
+        ],
+      ),
     );
   }
 }
