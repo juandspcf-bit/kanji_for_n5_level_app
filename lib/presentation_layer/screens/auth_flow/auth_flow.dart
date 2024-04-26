d2bea49d629111712b3f2aba229be0282f918c8f
Juan Carlos Aguilar Lopez <juandspcf@gmail.com>
2024-04-26 15:05:40 +0900

    added extra check in auth flow

--- lib/presentation_layer/screens/auth_flow/auth_flow.dart
+++ lib/presentation_layer/screens/auth_flow/auth_flow.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kanji_for_n5_level_app/aplication_layer/auth_service/auth_service_firebase.dart';
 import 'package:kanji_for_n5_level_app/aplication_layer/services.dart';
 import 'package:kanji_for_n5_level_app/presentation_layer/screens/auth_flow/verify_email.dart';
+import 'package:kanji_for_n5_level_app/presentation_layer/screens/common_screens/error_connection_screen.dart';
+import 'package:kanji_for_n5_level_app/presentation_layer/screens/common_screens/loading_screen.dart';
 import 'package:kanji_for_n5_level_app/presentation_layer/screens/login_screen/login_screen.dart';
 import 'package:kanji_for_n5_level_app/providers/score_kanji_list_provider.dart';
 import 'package:kanji_for_n5_level_app/providers/status_connection_provider.dart';
@@ -39,13 +41,22 @@ class _AutFlowState extends ConsumerState<AuthFlow> {
     return StreamBuilder(
         stream: streamAuth,
         builder: (ctx, snapShot) {
-          final user = snapShot.data;
-          if (user != null) {
-            ref.read(authServiceProvider).setLoggedUser();
-            return const VerifyEmail();
-          } else {
-            return LoginFormScreen();
+          if (snapShot.connectionState == ConnectionState.waiting) {
+            return const ProcessProgress(
+              message: 'Login to your account',
+            );
           }
+          if (snapShot.hasData) {
+            final user = snapShot.data;
+            if (user != null) {
+              ref.read(authServiceProvider).setLoggedUser();
+              return const VerifyEmail();
+            } else {
+              return LoginFormScreen();
+            }
+          }
+
+          return const ErrorConnectionScreen(message: 'Error while login');
         });
   }
 }
