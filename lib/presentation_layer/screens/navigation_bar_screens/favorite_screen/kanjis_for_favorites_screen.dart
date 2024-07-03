f9de998698ed3c9e5f82ae4c48291dd5cbbc9321
Juan Carlos Aguilar Lopez <juandspcf@gmail.com>
2024-07-03 22:30:57 +0900

    removed son unused providers

--- lib/presentation_layer/screens/navigation_bar_screens/favorite_screen/kanjis_for_favorites_screen.dart
+++ lib/presentation_layer/screens/navigation_bar_screens/favorite_screen/kanjis_for_favorites_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kanji_for_n5_level_app/providers/error_storing_database_status.dart';
 import 'package:kanji_for_n5_level_app/presentation_layer/screens/navigation_bar_screens/favorite_screen/favorites_kanjis_provider.dart';
 import 'package:kanji_for_n5_level_app/presentation_layer/screens/main_screens/main_content_provider.dart';
 import 'package:kanji_for_n5_level_app/presentation_layer/screens/kanji_list/body_list/body_list.dart';
@@ -14,18 +13,6 @@ class KanjisForFavoritesScreen extends ConsumerWidget with MyDialogs {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final mainScreenData = ref.watch(mainScreenProvider);
-    if (ref.watch(errorDatabaseStatusProvider)) {
-      errorDialog(
-        context,
-        () {
-          ref
-              .read(errorDatabaseStatusProvider.notifier)
-              .setDeletingError(false);
-        },
-        'An issue happened when deleting this item, please go back to the section'
-        ' list and access the content again to see the updated content.',
-      );
-    }
     var kanjiFavoritesList = ref.watch(favoritesKanjisProvider);
 
     return BodyKanjisList(
