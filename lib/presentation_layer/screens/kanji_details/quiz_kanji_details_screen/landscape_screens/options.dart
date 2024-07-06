99b96a88e7fc1643b4d85d6cc991716db885c4b3
Juan Carlos Aguilar Lopez <juandspcf@gmail.com>
2024-07-06 19:07:50 +0900

    changed animation to quiz screens

--- lib/presentation_layer/screens/kanji_details/quiz_kanji_details_screen/landscape_screens/options.dart
+++ lib/presentation_layer/screens/kanji_details/quiz_kanji_details_screen/landscape_screens/options.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kanji_for_n5_level_app/presentation_layer/screens/kanji_details/kanji_details_quiz_animated.dart';
+import 'package:kanji_for_n5_level_app/application_layer/services.dart';
 import 'package:kanji_for_n5_level_app/presentation_layer/screens/kanji_details/kanji_details_provider.dart';
+import 'package:kanji_for_n5_level_app/presentation_layer/screens/kanji_details/quiz_kanji_details_screen/flash_card/flash_card_quiz_provider.dart';
+import 'package:kanji_for_n5_level_app/presentation_layer/screens/kanji_details/quiz_kanji_details_screen/last_score_details_provider.dart';
+import 'package:kanji_for_n5_level_app/presentation_layer/screens/kanji_details/quiz_kanji_details_screen/last_score_flash_card_provider.dart';
+import 'package:kanji_for_n5_level_app/presentation_layer/screens/kanji_details/quiz_kanji_details_screen/quiz_details_main_screen.dart';
+import 'package:kanji_for_n5_level_app/presentation_layer/screens/kanji_details/quiz_kanji_details_screen/quiz_details_provider.dart';
+import 'package:kanji_for_n5_level_app/presentation_layer/screens/kanji_details/tabs_details/tab_media/video_widget/video_player_provider.dart';
+import 'package:kanji_for_n5_level_app/presentation_layer/screens/navigation_bar_screens/sections_screen/section_screen_provider.dart';
 
 class OptionsDetails extends ConsumerWidget {
   const OptionsDetails({super.key});
@@ -14,9 +21,56 @@ class OptionsDetails extends ConsumerWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        DetailsQuizScreenAnimated(
-          kanjiFromApi: kanjiFromApi,
-          closedChild: Container(
+        GestureDetector(
+          onTap: () {
+            ref.read(quizDetailsProvider.notifier).setDataQuiz(kanjiFromApi);
+            ref.read(quizDetailsProvider.notifier).setQuizState(0);
+
+            ref.read(flashCardProvider.notifier).initTheQuiz(kanjiFromApi);
+
+            ref
+                .read(lastScoreDetailsProvider.notifier)
+                .getSingleAudioExampleQuizDataDB(
+                  kanjiFromApi.kanjiCharacter,
+                  ref.read(sectionProvider),
+                  ref.read(authServiceProvider).userUuid ?? '',
+                );
+
+            ref
+                .read(lastScoreFlashCardProvider.notifier)
+                .getSingleFlashCardDataDB(
+                  kanjiFromApi.kanjiCharacter,
+                  ref.read(sectionProvider),
+                  ref.read(authServiceProvider).userUuid ?? '',
+                );
+
+            ref.read(quizDetailsProvider.notifier).setScreen(Screen.welcome);
+            ref.read(quizDetailsProvider.notifier).setOption(2);
+            ref.read(quizDetailsProvider.notifier).resetValues();
+
+            //pause video player
+
+            if (ref.read(videoPlayerObjectProvider).videoPlayerController !=
+                null) {
+              ref
+                  .read(videoPlayerObjectProvider)
+                  .videoPlayerController
+                  ?.pause();
+
+              ref.read(videoPlayerObjectProvider.notifier).setIsPlaying(false);
+            }
+
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return DetailsQuizScreen(
+                    kanjiFromApi: kanjiFromApi,
+                  );
+                },
+              ),
+            );
+          },
+          child: Container(
             width: 300,
             height: 40,
             decoration: BoxDecoration(
