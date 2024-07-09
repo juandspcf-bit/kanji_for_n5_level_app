17afd198e38546a9542146066cea097067c6173f
Juan Carlos Aguilar Lopez <juandspcf@gmail.com>
2024-07-09 17:03:02 +0900

    changed to shorter extension name

--- lib/presentation_layer/screens/navigation_bar_screens/list_kanjis_for_section_screen/kanjis_for_section_screen.dart
+++ lib/presentation_layer/screens/navigation_bar_screens/list_kanjis_for_section_screen/kanjis_for_section_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kanji_for_n5_level_app/l10n/localization.dart';
 import 'package:kanji_for_n5_level_app/presentation_layer/screens/navigation_bar_screens/list_kanjis_for_section_screen/connection_status_icon.dart';
 import 'package:kanji_for_n5_level_app/presentation_layer/screens/navigation_bar_screens/list_kanjis_for_section_screen/quiz_icon_kanji_list.dart';
 import 'package:kanji_for_n5_level_app/presentation_layer/screens/kanji_list/body_list/kanjis_list_provider.dart';
@@ -7,7 +8,6 @@ import 'package:kanji_for_n5_level_app/presentation_layer/screens/kanji_list/bod
 import 'package:kanji_for_n5_level_app/presentation_layer/common_widgets/my_dialogs.dart';
 
 import 'package:kanji_for_n5_level_app/presentation_layer/screens/kanji_list/body_list/status_operations_dialogs.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class KanjiForSectionScreen extends ConsumerWidget
     with MyDialogs, StatusDBStoringDialogs {
@@ -21,8 +21,7 @@ class KanjiForSectionScreen extends ConsumerWidget
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(AppLocalizations.of(context)!
-            .sections("section_${kanjiListData.section}")),
+        title: Text(context.l10n.sections("section_${kanjiListData.section}")),
         actions: const [
           ConnectionStatusIcon(),
           QuizIconKanjiList(),
